6b67247bc5faca36cd6f340b3d0fc40d9e47ef98
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2025-05-21 10:52:11 -0500

    fix:Modificar vista principal(Layaut)

--- app/src/main/java/com/menfroyt/studyoso/presentation/HomeScreen.kt
+++ app/src/main/java/com/menfroyt/studyoso/presentation/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.menfroyt.studyoso.presentation
 
+import android.R.id.primary
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,7 +13,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.AddCircleOutline
 import androidx.compose.material.icons.filled.Analytics
+import androidx.compose.material.icons.filled.Bedtime
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.NightsStay
 import androidx.compose.material.icons.filled.WbSunny
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -31,141 +35,216 @@ fun Home(navController: NavController) {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     var selectedScreen by remember { mutableStateOf("Principal") }
+    var isDarkTheme by remember { mutableStateOf(true) }
+    var valueFondo by remember { mutableStateOf(Color.White) }
 
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                DrawerContent(
-                    selectedScreen = selectedScreen, // Pasar selectedScreen
-                    onOptionSelected = { option ->
-                        when (option) {
-                            "Perfil" -> selectedScreen = "Perfil"
-                            "Dashboard" -> selectedScreen = "Dashboard"
-                            "Configuración" -> selectedScreen = "Configuración"
-                            "Pomodoro" -> selectedScreen = "Pomodoro"
-                            "Cerrar Sesión" -> navController.navigate("login") { popUpTo(0) }
-                        }
-                        scope.launch { drawerState.close() }
-                    }
-                )
-            }
+    MaterialTheme(
+        colorScheme = if (isDarkTheme) {
+            darkColorScheme(MaterialTheme.colorScheme.primary)
+        } else {
+            lightColorScheme(MaterialTheme.colorScheme.onPrimary)
         }
     ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text("StudyOsO", style = MaterialTheme.typography.titleLarge) },
-                    navigationIcon = {
-                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Menú",
-                                tint = MaterialTheme.colorScheme.onPrimary,
-                                modifier = Modifier.size(40.dp)
-                            )
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    DrawerContent(
+                        selectedScreen = selectedScreen, // Pasar selectedScreen
+                        onOptionSelected = { option ->
+                            when (option) {
+                                "Perfil" -> selectedScreen = "Perfil"
+                                "Dashboard" -> selectedScreen = "Dashboard"
+                                "Configuración" -> selectedScreen = "Configuración"
+                                "Pomodoro" -> selectedScreen = "Pomodoro"
+                                "Cerrar Sesión" -> navController.navigate("login") { popUpTo(0) }
+                            }
+                            scope.launch { drawerState.close() }
                         }
-                    },
-                    actions = {
-                        IconButton(onClick = { selectedScreen = "Principal" }) {
-                            Icon(
-                                imageVector = Icons.Filled.WbSunny,
-                                contentDescription = "cambio de fondo",
-                                tint = MaterialTheme.colorScheme.onPrimary,
-                                modifier = Modifier.size(40.dp)
-                            )
-                        }
-                        Spacer(modifier = Modifier.padding(8.dp))
-                        IconButton(onClick = { selectedScreen = "Perfil" }) {
-                            Icon(
-                                imageVector = Icons.Filled.AccountCircle,
-                                contentDescription = "Perfil",
-                                tint = MaterialTheme.colorScheme.onPrimary,
-                                modifier = Modifier.size(40.dp)
-                            )
-                        }
-                    },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        titleContentColor = MaterialTheme.colorScheme.onPrimary
                     )
-                )
-            },
-            bottomBar = {
-                BottomAppBar(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary,
-                    modifier = Modifier.height(110.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        IconButton(onClick = { selectedScreen = "Dashboard" }, modifier = Modifier.size(48.dp)) {
-                            Icon(
-                                imageVector = Icons.Filled.Analytics,
-                                contentDescription = "dashboard",
-                                tint = if (selectedScreen == "Dashboard") Color(0xFFFFA500) else Color.White,
-                                modifier = Modifier.size(55.dp)
-                            )
-                        }
-                        IconButton(onClick = { selectedScreen = "Pomodoro" }, modifier = Modifier.size(48.dp)) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.book_clock),
-                                contentDescription = "pomodoro",
-                                tint = if (selectedScreen == "Pomodoro") Color(0xFFFFA500) else Color.White,
-                                modifier = Modifier.size(55.dp)
-                            )
-                        }
-                        IconButton(onClick = { selectedScreen = "Principal" }, modifier = Modifier.size(48.dp)) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.home),
-                                contentDescription = "home",
-                                tint = if (selectedScreen == "Principal") Color(0xFFFFA500) else Color.White,
-                                modifier = Modifier.size(55.dp)
-                            )
-                        }
-                        IconButton(onClick = { selectedScreen = "Calificaciones" }, modifier = Modifier.size(48.dp)) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.calificacion),
-                                contentDescription = "calificaciones",
-                                tint = if (selectedScreen == "Calificaciones") Color(0xFFFFA500) else Color.White,
-                                modifier = Modifier.size(55.dp)
-                            )
-                        }
-                        IconButton(onClick = { selectedScreen = "ListaTareas" }, modifier = Modifier.size(48.dp)) {
-                            Icon(
-                                imageVector = Icons.Filled.AddCircleOutline,
-                                contentDescription = "lista_tareas",
-                                tint = if (selectedScreen == "ListaTareas") Color(0xFFFFA500) else Color.White,
-                                modifier = Modifier.size(55.dp)
-                            )
+                }
+            }
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text("StudyOsO", style = MaterialTheme.typography.titleLarge) },
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Menu,
+                                    contentDescription = "Menú",
+                                    tint = MaterialTheme.colorScheme.onPrimary,
+                                    modifier = Modifier.size(30.dp)
+                                )
+                            }
+                        },
+                        actions = {
+                            IconButton(onClick = { isDarkTheme = !isDarkTheme }) {
+                                Icon(
+                                    imageVector = if (isDarkTheme) Icons.Filled.WbSunny else Icons.Filled.Bedtime,
+                                    contentDescription = "cambiar tema",
+                                    tint = MaterialTheme.colorScheme.onPrimary,
+                                    modifier = Modifier.size(30.dp)
+                                )
+                            }
+                            Spacer(modifier = Modifier.padding(8.dp))
+                            IconButton(onClick = { selectedScreen = "Perfil" }) {
+                                Icon(
+                                    imageVector = Icons.Filled.AccountCircle,
+                                    contentDescription = "Perfil",
+                                    tint = MaterialTheme.colorScheme.onPrimary,
+                                    modifier = Modifier.size(30.dp)
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary
+
+                        )
+                    )
+                },
+                bottomBar = {
+                    BottomAppBar(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+
+                        ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            NavigationBar(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                contentColor = MaterialTheme.colorScheme.onPrimary
+                            ) {
+                                NavigationBarItem(
+                                    selected = selectedScreen == "Dashboard",
+                                    onClick = { selectedScreen = "Dashboard" },
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Analytics,
+                                            contentDescription = "dashboard",
+                                            modifier = Modifier.size(30.dp)
+                                        )
+                                    },
+                                    label = { Text("Dashboard") },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        selectedIconColor = Color(0xFFFFA500),
+                                        unselectedIconColor = Color.White,
+                                        selectedTextColor = Color.White,
+                                        unselectedTextColor = Color.White
+                                    )
+                                )
+
+                                NavigationBarItem(
+                                    selected = selectedScreen == "Pomodoro",
+                                    onClick = { selectedScreen = "Pomodoro" },
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.book_clock),
+                                            contentDescription = "pomodoro",
+                                            modifier = Modifier.size(30.dp)
+                                        )
+                                    },
+                                    label = { Text("Pomodoro") },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        selectedIconColor = Color(0xFFFFA500),
+                                        unselectedIconColor = Color.White,
+                                        selectedTextColor = Color.White,
+                                        unselectedTextColor = Color.White
+                                    )
+                                )
+
+                                NavigationBarItem(
+                                    selected = selectedScreen == "Principal",
+                                    onClick = { selectedScreen = "Principal" },
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.home),
+                                            contentDescription = "home",
+                                            modifier = Modifier.size(30.dp)
+                                        )
+                                    },
+                                    label = { Text("Inicio") },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        selectedIconColor = Color(0xFFFFA500),
+                                        unselectedIconColor = Color.White,
+                                        selectedTextColor = Color.White,
+                                        unselectedTextColor = Color.White
+                                    )
+                                )
+
+                                NavigationBarItem(
+                                    selected = selectedScreen == "Calificaciones",
+                                    onClick = { selectedScreen = "Calificaciones" },
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.calificacion),
+                                            contentDescription = "calificaciones",
+                                            modifier = Modifier.size(30.dp)
+                                        )
+                                    },
+                                    label = { Text("Notas") },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        selectedIconColor = Color(0xFFFFA500),
+                                        unselectedIconColor = Color.White,
+                                        selectedTextColor = Color.White,
+                                        unselectedTextColor = Color.White
+                                    )
+                                )
+
+                                NavigationBarItem(
+                                    selected = selectedScreen == "ListaTareas",
+                                    onClick = { selectedScreen = "ListaTareas" },
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.AddCircleOutline,
+                                            contentDescription = "lista_tareas",
+                                            modifier = Modifier.size(30.dp)
+                                        )
+                                    },
+                                    label = { Text("Lista Tareas") },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        selectedIconColor = Color(0xFFFFA500),
+                                        unselectedIconColor = Color.White,
+                                        selectedTextColor = Color.White,
+                                        unselectedTextColor = Color.White
+                                    )
+                                )
+                            }
+
                         }
                     }
                 }
-            }
-        ) { innerPadding ->
-            when (selectedScreen) {
-                "Principal" -> PrincipalScreen(
-                    modifier = Modifier.padding(innerPadding),
-                    onScreenSelected = { screen -> selectedScreen = screen }
-                )
-                "Dashboard" -> DashboardScreen(
-                    modifier = Modifier.padding(innerPadding),
-                    onScreenSelected = { screen -> selectedScreen = screen }
-                )
-                "Perfil" -> PerfilScreen(Modifier.padding(innerPadding))
-                "Configuración" -> ConfiguracionScreen(Modifier.padding(innerPadding))
-                "Pomodoro" -> PomodoroScreen(Modifier.padding(innerPadding))
-                "AgregarCursos" -> AgregarCursosScreen(Modifier.padding(innerPadding))
-                "Calificaciones" -> CalificacionesScreen(Modifier.padding(innerPadding))
-                "ListaTareas" -> ListaTareasScreen(Modifier.padding(innerPadding))
-                "MatrizEisenhower" -> MatrizEisenhowerScreen(Modifier.padding(innerPadding))
-                "Calendario" -> CalendarioScreen(Modifier.padding(innerPadding))
+            ) { innerPadding ->
+                when (selectedScreen) {
+                    "Principal" -> PrincipalScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onScreenSelected = { screen -> selectedScreen = screen }
+                    )
+
+                    "Dashboard" -> DashboardScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onScreenSelected = { screen -> selectedScreen = screen }
+                    )
+
+                    "Perfil" -> PerfilScreen(Modifier.padding(innerPadding))
+                    "Configuración" -> ConfiguracionScreen(Modifier.padding(innerPadding))
+                    "Pomodoro" -> PomodoroScreen(Modifier.padding(innerPadding))
+                    "AgregarCursos" -> AgregarCursosScreen(Modifier.padding(innerPadding))
+                    "Calificaciones" -> CalificacionesScreen(Modifier.padding(innerPadding))
+                    "ListaTareas" -> ListaTareasScreen(Modifier.padding(innerPadding))
+                    "MatrizEisenhower" -> MatrizEisenhowerScreen(Modifier.padding(innerPadding))
+                    "Calendario" -> CalendarioScreen(Modifier.padding(innerPadding))
+                }
             }
         }
     }
+
 }
 
 
