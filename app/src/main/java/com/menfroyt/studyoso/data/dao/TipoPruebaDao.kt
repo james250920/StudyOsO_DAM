c73e38d8cc4662ab66ca5e539f5351f9ac2962fa
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2025-06-11 10:31:53 -0500

    feat(database): add repositories and entities for Cursos, Calificaciones, Tareas, and Tipos de Prueba

---
+++ app/src/main/java/com/menfroyt/studyoso/data/dao/TipoPruebaDao.kt
@@ -0,0 +1,38 @@
+package com.menfroyt.studyoso.data.dao
+
+import androidx.room.Dao
+import androidx.room.*
+import com.menfroyt.studyoso.data.entities.TipoPrueba
+import esan.mendoza.teststudyoso.domain.CursoConTiposPrueba
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface TipoPruebaDao {
+    @Insert
+    suspend fun insert(tipoPrueba: TipoPrueba): Long
+
+    @Update
+    suspend fun update(tipoPrueba: TipoPrueba)
+
+    @Delete
+    suspend fun delete(tipoPrueba: TipoPrueba)
+
+    @Query("SELECT * FROM Tipos_Prueba WHERE id_curso = :cursoId")
+    fun getTiposByCurso(cursoId: Int): Flow<List<TipoPrueba>>
+
+    //prueba
+    @Query("""
+    SELECT 
+        c.id_curso AS idCurso,
+        tp.nombre_tipo AS nombreTipo,
+        tp.cantidad_pruebas AS cantidadPruebas,
+        tp.peso_total AS pesoTotal
+    FROM Cursos c
+    INNER JOIN Tipos_Prueba tp ON c.id_curso = tp.id_curso
+    WHERE c.id_curso = :cursoId
+""")
+    fun getTiposPruebaByCurso(cursoId: Int): Flow<List<CursoConTiposPrueba>>
+
+
+}
---
+++ app/src/main/java/com/menfroyt/studyoso/data/repositories/TipoPruebaRepository.kt
@@ -0,0 +1,20 @@
+package com.menfroyt.studyoso.data.repositories
+
+import com.menfroyt.studyoso.data.dao.TipoPruebaDao
+import com.menfroyt.studyoso.data.entities.TipoPrueba
+import esan.mendoza.teststudyoso.domain.CursoConTiposPrueba
+import kotlinx.coroutines.flow.Flow
+
+class TipoPruebaRepository(private val dao: TipoPruebaDao) {
+    fun getTiposByCurso(cursoId: Int): Flow<List<TipoPrueba>> = dao.getTiposByCurso(cursoId)
+
+    suspend fun insert(tipoPrueba: TipoPrueba) = dao.insert(tipoPrueba)
+
+    suspend fun update(tipoPrueba: TipoPrueba) = dao.update(tipoPrueba)
+
+    suspend fun delete(tipoPrueba: TipoPrueba) = dao.delete(tipoPrueba)
+
+    fun getTiposPruebaByCurso(cursoId: Int): Flow<List<CursoConTiposPrueba>> {
+        return dao.getTiposPruebaByCurso(cursoId)
+    }
+}
