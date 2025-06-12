4f9fbc95de7c038024e0be69b8387b20ce24e4dd
Benny <bennycm.13@gmail.com>
2025-06-12 01:25:22 -0500

    Remove unused imports in CalificacionesScreen

--- app/src/main/java/com/menfroyt/studyoso/presentation/calificación/CalificacionesScreen.kt
+++ app/src/main/java/com/menfroyt/studyoso/presentation/calificación/CalificacionesScreen.kt
@@ -16,13 +16,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.menfroyt.studyoso.domain.TipoPrueba
+
 @Composable
 fun CalificacionesScreen(modifier: Modifier = Modifier, onScreenSelected: (String) -> Unit) {
     Column(
