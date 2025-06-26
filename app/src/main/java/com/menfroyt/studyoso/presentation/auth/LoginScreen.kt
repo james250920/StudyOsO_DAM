209088b4d17204f00b40a8d34143af66495b3fcc
unknown <chancasevangelistagerson@gmail.com>
2025-06-25 22:31:07 -0500

    ui: Rediseñar pantallas de inicio de sesión y registro

--- app/src/main/java/com/menfroyt/studyoso/presentation/auth/LoginScreen.kt
+++ app/src/main/java/com/menfroyt/studyoso/presentation/auth/LoginScreen.kt
@@ -1,7 +1,10 @@
 package com.menfroyt.studyoso.presentation.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,13 +27,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.menfroyt.studyoso.R
 import com.menfroyt.studyoso.ViewModel.usuario.UsuarioViewModel
 import com.menfroyt.studyoso.ViewModel.usuario.UsuarioViewModelFactory
 import com.menfroyt.studyoso.data.db.AppDatabase
@@ -73,73 +79,97 @@ fun LoginScreen(
     }
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Iniciar Sesión") }
-            )
-        },
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { padding ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedTextField(
-                value = correo,
-                onValueChange = { correo = it },
-                label = { Text("Correo Electrónico") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                modifier = Modifier.fillMaxWidth()
+            Image(
+                painter = painterResource(id = R.drawable.backgroundlogin),
+                contentDescription = "Background Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop // O ContentScale.FillBounds según el efecto deseado
             )
-
-            OutlinedTextField(
-                value = contrasena,
-                onValueChange = { contrasena = it },
-                label = { Text("Contraseña") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Button(
-                onClick = {
-                    if (correo.text.isBlank() || contrasena.text.isBlank()) {
-                        errorMessage = "Ingrese correo y contraseña"
-                        showError = true
-                        return@Button
-                    }
-
-                    usuarioViewModel.login(
-                        correo.text.trim(),
-                        contrasena.text.trim(),
-                        onSuccess = { usuario ->
-                            // Guardar la sesión
-                            sessionManager.saveSession(usuario.idUsuario, correo.text.trim())
-                            navController.navigate("home/${usuario.idUsuario}") {
-                                popUpTo("login") { inclusive = true }
-                            }
-                        },
-                        onError = { mensaje ->
-                            errorMessage = mensaje
-                            showError = true
-                        }
-                    )
-                },
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center, // Modificado para centrar verticalmente
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Iniciar Sesión")
-            }
 
-            TextButton(
-                onClick = { navController.navigate("register") }
-            ) {
-                Text("¿No tienes cuenta? Regístrate")
+                Image(
+                    painter = painterResource(id = R.drawable.study),
+                    contentDescription = "Study OSO Logo",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .align(Alignment.CenterHorizontally),
+                    contentScale = ContentScale.Fit
+                )
+
+                OutlinedTextField(
+                    value = correo,
+                    onValueChange = { correo = it },
+                    label = { Text("Correo Electrónico") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp) // Añade padding horizontal
+                )
+                Spacer(modifier = Modifier.height(8.dp)) // Espacio entre los campos
+                OutlinedTextField(
+                    value = contrasena,
+                    onValueChange = { contrasena = it },
+                    label = { Text("Contraseña") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp) // Añade padding horizontal
+                )
+                Spacer(modifier = Modifier.height(16.dp)) // Espacio antes del botón
+                Button(
+                    onClick = {
+                        if (correo.text.isBlank() || contrasena.text.isBlank()) {
+                            errorMessage = "Ingrese correo y contraseña"
+                            showError = true
+                            return@Button
+                        }
+
+                        usuarioViewModel.login(
+                            correo.text.trim(),
+                            contrasena.text.trim(),
+                            onSuccess = { usuario ->
+                                // Guardar la sesión
+                                sessionManager.saveSession(usuario.idUsuario, correo.text.trim())
+                                navController.navigate("home/${usuario.idUsuario}") {
+                                    popUpTo("login") { inclusive = true }
+                                }
+                            },
+                            onError = { mensaje ->
+                                errorMessage = mensaje
+                                showError = true
+                            }
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f) // El botón ocupará el 60% del ancho disponible
+                        .height(50.dp)
+                        .align(Alignment.CenterHorizontally) // Centra el botón horizontalmente
+                ) {
+                    Text("Iniciar Sesión")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp)) // Espacio antes del TextButton
+                TextButton(
+                    onClick = { navController.navigate("register") },
+                    modifier = Modifier.align(Alignment.CenterHorizontally) // Centra el TextButton
+                ) {
+                    Text("¿No tienes cuenta? Regístrate")
+                }
             }
         }
     }
--- app/src/main/java/com/menfroyt/studyoso/presentation/auth/RegisterScreen.kt
+++ app/src/main/java/com/menfroyt/studyoso/presentation/auth/RegisterScreen.kt
@@ -1,26 +1,33 @@
 package com.menfroyt.studyoso.presentation.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.menfroyt.studyoso.R
 import com.menfroyt.studyoso.ViewModel.usuario.UsuarioViewModel
 import com.menfroyt.studyoso.ViewModel.usuario.UsuarioViewModelFactory
 import com.menfroyt.studyoso.data.db.AppDatabase
 import com.menfroyt.studyoso.data.entities.Usuario
 import com.menfroyt.studyoso.data.repositories.UsuarioRepository
 import java.sql.Date
-import java.util.*
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,8 +72,9 @@ fun RegisterScreen(
             onDismissRequest = { showDatePicker = false },
             confirmButton = {
                 TextButton(onClick = {
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
                     datePickerState.selectedDateMillis?.let { millis ->
-                        val date = Date(millis)
+                        val date = Date(millis + 86400000) // Sumar un día en milisegundos para corregir la fecha
                         fechaNacimiento = TextFieldValue(date.toString())
                     }
                     showDatePicker = false
@@ -85,100 +93,151 @@ fun RegisterScreen(
     }
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Registro de Usuario") }
-            )
-        },
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { padding ->
-        Column(
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            OutlinedTextField(
-                value = nombre,
-                onValueChange = { nombre = it },
-                label = { Text("Nombre") },
-                modifier = Modifier.fillMaxWidth()
+            // Imagen de fondo
+            Image(
+                painter = painterResource(id = R.drawable.background), // Usar la imagen c1.png
+                contentDescription = "Fondo de registro",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop // Ajustar para que cubra toda la pantalla
             )
 
-            OutlinedTextField(
-                value = apellido,
-                onValueChange = { apellido = it },
-                label = { Text("Apellido") },
-                modifier = Modifier.fillMaxWidth()
-            )
+            // Contenido de la pantalla
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center, // Centrar verticalmente
+                horizontalAlignment = Alignment.CenterHorizontally // Centrar horizontalmente
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.study),
+                    contentDescription = "Study OSO Logo",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .padding(bottom = 16.dp), // Espacio debajo del logo
+                    contentScale = ContentScale.Fit
+                )
 
-            OutlinedTextField(
-                value = correo,
-                onValueChange = { correo = it },
-                label = { Text("Correo Electrónico") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                modifier = Modifier.fillMaxWidth()
-            )
+                //texto Crear una cuenta
+                Text(
+                    text = "Crear una cuenta",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(bottom = 16.dp) // Espacio debajo del título
+                )
 
-            OutlinedTextField(
-                value = contrasena,
-                onValueChange = { contrasena = it },
-                label = { Text("Contraseña") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth()
-            )
+                OutlinedTextField(
+                    value = nombre,
+                    onValueChange = { nombre = it },
+                    label = { Text("Nombre") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp) // Espacio debajo del campo
+                        .padding(horizontal = 16.dp)
+                )
 
-            OutlinedTextField(
-                value = fechaNacimiento,
-                onValueChange = { fechaNacimiento = it },
-                label = { Text("Fecha de Nacimiento") },
-                modifier = Modifier.fillMaxWidth(),
-                readOnly = true,
-                trailingIcon = {
-                    IconButton(onClick = { showDatePicker = true }) {
-                        Icon(
-                            imageVector = Icons.Default.DateRange,
-                            contentDescription = "Seleccionar fecha"
+                OutlinedTextField(
+                    value = apellido,
+                    onValueChange = { apellido = it },
+                    label = { Text("Apellido") },
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(bottom = 8.dp) // Espacio debajo del campo
+                        .padding(horizontal = 16.dp)
+                )
+
+
+                OutlinedTextField(
+                    value = correo,
+                    onValueChange = { correo = it },
+                    label = { Text("Correo Electrónico") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(bottom = 8.dp) // Espacio debajo del campo
+                        .padding(horizontal = 16.dp)
+                )
+
+                OutlinedTextField(
+                    value = contrasena,
+                    onValueChange = { contrasena = it },
+                    label = { Text("Contraseña") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(bottom = 8.dp) // Espacio debajo del campo
+                        .padding(horizontal = 16.dp)
+                )
+
+                OutlinedTextField(
+                    value = fechaNacimiento,
+                    onValueChange = { fechaNacimiento = it },
+                    label = { Text("Fecha de Nacimiento") },
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(bottom = 16.dp) // Espacio debajo del campo
+                        .padding(horizontal = 16.dp),
+                    readOnly = true,
+                    trailingIcon = {
+                        IconButton(onClick = { showDatePicker = true }) {
+                            Icon(
+                                imageVector = Icons.Default.DateRange,
+                                contentDescription = "Seleccionar fecha"
+                            )
+                        }
+                    }
+                )
+
+                //Agregar texto de terminos y condiciones.
+                Text(
+                    text = "Al hacer clic en Registrarte, aceptas las Condiciones, la Política de privacidad y la Política de cookies.",
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(horizontal = 24.dp).padding(bottom = 16.dp) // Espacio debajo del texto
+                )
+
+                Button(
+                    onClick = {
+                        if (nombre.text.isBlank() || apellido.text.isBlank() ||
+                            correo.text.isBlank() || contrasena.text.isBlank() ||
+                            fechaNacimiento.text.isBlank()
+                        ) {
+                            errorMessage = "Complete todos los campos"
+                            showError = true
+                            return@Button
+                        }
+
+                        val usuario = Usuario(
+                            nombre = nombre.text.trim(),
+                            apellido = apellido.text.trim(),
+                            correo = correo.text.trim(),
+                            contrasena = contrasena.text.trim(),
+                            fechaNacimiento = fechaNacimiento.text.trim()
                         )
-                    }
+                        usuarioViewModel.agregarUsuario(usuario)
+                        navController.navigate("login") {
+                            popUpTo("register") { inclusive = true }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp) // Espacio debajo del botón
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Text("Registrarse")
                 }
-            )
 
-            Button(
-                onClick = {
-                    navController.navigate("login")
-                    if (nombre.text.isBlank() || apellido.text.isBlank() ||
-                        correo.text.isBlank() || contrasena.text.isBlank() ||
-                        fechaNacimiento.text.isBlank()
-                    ) {
-                        errorMessage = "Complete todos los campos"
-                        showError = true
-                        return@Button
-                    }
-
-                    val usuario = Usuario(
-                        nombre = nombre.text.trim(),
-                        apellido = apellido.text.trim(),
-                        correo = correo.text.trim(),
-                        contrasena = contrasena.text.trim(),
-                        fechaNacimiento = fechaNacimiento.text.trim()
-                    )
-                    usuarioViewModel.agregarUsuario(usuario)
-                    navController.navigate("login") {
-                        popUpTo("register") { inclusive = true }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Registrarse")
-            }
-
-            TextButton(
-                onClick = { navController.navigate("login") }
-            ) {
-                Text("¿Tienes cuenta? Iniciar Sesión")
+                TextButton(
+                    modifier = Modifier.align(Alignment.CenterHorizontally), // Centrar el botón de texto
+                    onClick = { navController.navigate("login") }
+                ) {
+                    Text("¿Tienes cuenta? Iniciar Sesión")
+                }
             }
         }
     }
