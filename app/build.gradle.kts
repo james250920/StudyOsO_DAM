cac78cae7d5bc837e48bba07948d695c214fbf80
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2025-07-18 23:18:55 -0500

    fix: actualizar minSdk a 28 en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.menfroyt.studyoso"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
